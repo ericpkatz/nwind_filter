75b2aae19d795903fb3cc280009f23d1086aa454
Eric P. Katz <ericpkatz@gmail.com>
2016-04-14 05:49:11 -0400

    get tests set up

---
+++ test/browser/app.spec.js
@@ -0,0 +1,9 @@
+describe('app', function(){
+  beforeEach(module('app'));
+
+  describe('the app exists', function(){
+    it('exists', function(){
+      expect(true).to.equal(true);
+    });
+  });
+});
